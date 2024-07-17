42dc19c70cf0e9ebe59ff52da77083f4250f8470
akhiljoji1451 <akhiljoji1451@gmail.com>
2024-07-17 08:14:18 +0000

    modified

--- chruch.sql
+++
@@ -1,5 +0,0 @@
--- Suggested code may be subject to a license. Learn more: ~LicenseLog:3598323754.
-CREATE TABLE carouselimages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image_path VARCHAR(255) NOT NULL
-);
